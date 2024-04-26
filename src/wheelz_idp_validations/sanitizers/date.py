e9a5d7eda982636d9f4e1b889590390b96f72018
raulvazquez-wheelz <148226717+raulvazquez-wheelz@users.noreply.github.com>
2024-04-26 12:06:55 +0200

    Update date.py

--- src/wheelz_idp_validations/sanitizers/date.py
+++ src/wheelz_idp_validations/sanitizers/date.py
@@ -1,19 +1,30 @@
 import re
+import logging
+from datetime import datetime
 
+# Configurar logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def sanitize_dates(received_birth_date, received_expiration_date):
+def sanitize_dates(text):
+    # Comprobar si la entrada es una cadena vacía o solo contiene espacios
+    if not text.strip():
+        logging.info("Empty input received for date.")
+        return ""  # Retornar cadena vacía directamente si no hay entrada
 
-    # TODO Change format yyyy mm dd
-    # Dates must match this pattern
-    pattern = re.compile(r'\d{2}/\d{2}/\d{4}')
-
-    processed_birth_date = pattern.search(received_birth_date)
-    processed_expiration_date = pattern.search(received_expiration_date)
-
-    if not processed_birth_date:
-        raise InvalidBirthDateException(processed_birth_date)
-
-    if not processed_expiration_date:
-        raise InvalidExpirationDateException(processed_birth_date)
-
-    return processed_birth_date.group(), processed_expiration_date.group()
+    date_pattern = r'\b(\d{2})[\/\s-](\d{2})[\/\s-](\d{4})\b'
+    
+    try:
+        match = re.search(date_pattern, text)
+        if match:
+            day, month, year = match.groups()
+            iso_formatted_date = f"{year}-{month}-{day}"
+            datetime.strptime(iso_formatted_date, "%Y-%m-%d")  # Validar fecha
+            return iso_formatted_date
+        else:
+            # Registra que no se encontró una fecha válida
+            logging.warning(f"No valid date found in text: '{text}'")
+            return ""  # Retornar cadena vacía si no se encuentra fecha
+    except ValueError as e:
+        # Registra el error específico con la fecha incorrecta
+        logging.error(f"Invalid date format or logic error: {e}")
+        return ""  # Retornar cadena vacía si la fecha es inválida
