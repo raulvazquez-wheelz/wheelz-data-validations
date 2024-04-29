2d53bf3237ded865f0604dc4ae2b78f1228e3caa
raulvazquez-wheelz <148226717+raulvazquez-wheelz@users.noreply.github.com>
2024-04-29 14:18:27 +0200

    Update date.py

--- src/wheelz_idp_validations/sanitizers/date.py
+++ src/wheelz_idp_validations/sanitizers/date.py
@@ -1,18 +1,25 @@
 import re
+from datetime import datetime
+from ..exceptions.date_exceptions import *
 
-def sanitize_dates(received_birth_date, received_expiration_date):
+def sanitize_dates(received_date):
+    # Comprobar si la entrada es una cadena vacía o solo contiene espacios
+    if not received_date.strip():
+        raise EmptyDateException()
 
-    # TODO Change format yyyy mm dd
-    # Dates must match this pattern
-    pattern = re.compile(r'\d{2}/\d{2}/\d{4}')
-
-    processed_birth_date = pattern.search(received_birth_date)
-    processed_expiration_date = pattern.search(received_expiration_date)
-
-    if not processed_birth_date:
-        raise InvalidBirthDateException(processed_birth_date)
-
-    if not processed_expiration_date:
-        raise InvalidExpirationDateException(processed_birth_date)
-
-    return processed_birth_date.group(), processed_expiration_date.group()
+    # Patrón para encontrar la fecha en distintos formatos comunes
+    date_pattern = r'\b(\d{2})[\/\s-](\d{2})[\/\s-](\d{4})\b'
+    
+    try:
+        match = re.search(date_pattern, received_date)
+        if match:
+            day, month, year = match.groups()
+            iso_formatted_date = f"{year}-{month}-{day}"
+            datetime.strptime(iso_formatted_date, "%Y-%m-%d")  # Validar fecha convertida
+            return iso_formatted_date
+        else:
+            # Lanzar una excepción si no se encuentra una fecha válida
+            raise InvalidDateException(received_date)
+    except ValueError as e:
+        # Lanzar una excepción si la fecha es inválida o hay un error lógico
+        raise InvalidDateException(received_date)
