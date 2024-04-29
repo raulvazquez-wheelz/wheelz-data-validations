ef8da7f10233808f743b043411cd50c2e5fd2760
raulvazquez-wheelz <148226717+raulvazquez-wheelz@users.noreply.github.com>
2024-04-29 16:07:05 +0200

    Update id_sanitizer.py

--- src/wheelz_idp_validations/id_sanitizer.py
+++ src/wheelz_idp_validations/id_sanitizer.py
@@ -2,14 +2,33 @@ from wheelz_idp_validations.sanitizers.id_number import sanitize_id_number
 from wheelz_idp_validations.sanitizers.gender import sanitize_gender
 from wheelz_idp_validations.sanitizers.date import sanitize_dates
 
-
 def sanitize_id(response_dict):
     # Sanitize ID Number
-    response_dict["documentNumber"] = sanitize_id_number(response_dict["documentNumber"], False)
+    try: 
+        response_dict["documentNumber"] = sanitize_id_number(response_dict["documentNumber"], False)
+    except Exception as e:
+        response_dict["documentNumber"] = ""
+        print("Error sanitizing document number:", str(e))
     
     # Sanitize Gender
-    response_dict["gender"] = sanitize_gender(response_dict["gender"])
+    try:
+        response_dict["gender"] = sanitize_gender(response_dict["gender"])
+    except Exception as e:
+        response_dict["gender"] = ""
+        print("Error sanitizing gender:", str(e))
     
     # Sanitize Birth Date and Expiration Date
-    response_dict["birthDate"] = sanitize_dates(response_dict["birthDate"])
-    response_dict["expirationDate"] = sanitize_dates(response_dict["expirationDate"])
+    try:
+        response_dict["birthDate"] = sanitize_dates(response_dict["birthDate"])
+    except Exception as e:
+        response_dict["birthDate"] = ""
+        print("Error sanitizing birth date:", str(e))
+
+    try:
+        response_dict["expirationDate"] = sanitize_dates(response_dict["expirationDate"])
+    except Exception as e:
+        response_dict["expirationDate"] = ""
+        print("Error sanitizing expiration date:", str(e))
+
+    return response_dict
+
