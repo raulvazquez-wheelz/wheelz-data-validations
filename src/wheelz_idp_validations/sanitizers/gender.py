1818a5c1d25dd4cd48a9ca231f270668794a4081
raulvazquez-wheelz <148226717+raulvazquez-wheelz@users.noreply.github.com>
2024-04-29 13:38:56 +0200

    Update gender.py

--- src/wheelz_idp_validations/sanitizers/gender.py
+++ src/wheelz_idp_validations/sanitizers/gender.py
@@ -1,11 +1,15 @@
 import re
-
+from ..exceptions.gender_exceptions import *
 
 def sanitize_gender(received_gender):
-    # Check if gender is correct (M or F separated with boundaries)
-    match = re.search(r'[MF]', received_gender)
+    # Use regex to find an 'M' or 'F' anywhere in the string
+    match = re.search(r'\b[MF]\b', received_gender)
 
     if not match:
-        raise InvalidGenderException(received_gender)
+        # If no standalone 'M' or 'F' is found, try finding 'M' or 'F' surrounded by other characters
+        match = re.search(r'[MF]', received_gender)
+        if not match:
+            # If still not found, raise an exception
+            raise InvalidGenderException(received_gender)
 
     return match.group()
