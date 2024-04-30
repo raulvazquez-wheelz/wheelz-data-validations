364423f6c6e679959d0b6634c6ffe925e4b3eb04
raulvazquez-wheelz <148226717+raulvazquez-wheelz@users.noreply.github.com>
2024-04-30 13:11:58 +0200

    Update id_sanitizer.py

--- src/wheelz_idp_validations/id_sanitizer.py
+++ src/wheelz_idp_validations/id_sanitizer.py
@@ -4,31 +4,43 @@ from wheelz_idp_validations.sanitizers.date import sanitize_dates
 
 def sanitize_id(response_dict):
     # Sanitize ID Number
-    try: 
-        response_dict["documentNumber"] = sanitize_id_number(response_dict["documentNumber"], False)
-    except Exception as e:
-        response_dict["documentNumber"] = ""
-        print("Error sanitizing document number:", str(e))
-    
-    # Sanitize Gender
-    try:
-        response_dict["gender"] = sanitize_gender(response_dict["gender"])
-    except Exception as e:
-        response_dict["gender"] = ""
-        print("Error sanitizing gender:", str(e))
-    
-    # Sanitize Birth Date and Expiration Date
-    try:
-        response_dict["birthDate"] = sanitize_dates(response_dict["birthDate"])
-    except Exception as e:
-        response_dict["birthDate"] = ""
-        print("Error sanitizing birth date:", str(e))
+    if "documentNumber" in response_dict:
+        try: 
+            response_dict["documentNumber"] = sanitize_id_number(response_dict["documentNumber"], True)
+        except Exception as e:
+            response_dict["documentNumber"] = ""
+            print("Error sanitizing document number:", str(e))
+    else:
+        print("Warning: 'documentNumber' has not been validated because not found in the response dictionary.")
 
-    try:
-        response_dict["expirationDate"] = sanitize_dates(response_dict["expirationDate"])
-    except Exception as e:
-        response_dict["expirationDate"] = ""
-        print("Error sanitizing expiration date:", str(e))
+    # Sanitize Gender
+    if "gender" in response_dict:
+        try:
+            response_dict["gender"] = sanitize_gender(response_dict["gender"])
+        except Exception as e:
+            response_dict["gender"] = ""
+            print("Error sanitizing gender:", str(e))
+    else:
+        print("Warning: 'gender' has not been validated because not found in the response dictionary.")
+
+    # Sanitize Birth Date
+    if "birthDate" in response_dict:
+        try:
+            response_dict["birthDate"] = sanitize_dates(response_dict["birthDate"])
+        except Exception as e:
+            response_dict["birthDate"] = ""
+            print("Error sanitizing birth date:", str(e))
+    else:
+        print("Warning: 'birthDate' has not been validated because not found in the response dictionary.")
+
+    # Sanitize Expiration Date
+    if "expirationDate" in response_dict:
+        try:
+            response_dict["expirationDate"] = sanitize_dates(response_dict["expirationDate"])
+        except Exception as e:
+            response_dict["expirationDate"] = ""
+            print("Error sanitizing expiration date:", str(e))
+    else:
+        print("Warning: 'expirationDate' has not been validated because not found in the response dictionary.")
 
     return response_dict
-
