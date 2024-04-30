8c6bb6f7a48a009c8aae4795f8f6cc36c1241095
raulvazquez-wheelz <148226717+raulvazquez-wheelz@users.noreply.github.com>
2024-04-30 15:28:55 +0200

    Update vin.py

--- src/wheelz_idp_validations/sanitizers/vin.py
+++ src/wheelz_idp_validations/sanitizers/vin.py
@@ -2,15 +2,22 @@ import re
 from ..exceptions.id_exceptions import *
 
 def calculate_check_digit(vin):
-    # Mapeo de letras a valores para cálculo del VIN
-    vin_map = "0123456789.ABCDEFGH..JKLMN.P.R..STUVWXYZ"
+    # Mapeo de letras a valores para cálculo del VIN según la norma ISO 3779
+    vin_values = {
+        '0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
+        'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8,
+        'J': 1, 'K': 2, 'L': 3, 'M': 4, 'N': 5, 'P': 7, 'R': 9,
+        'S': 2, 'T': 3, 'U': 4, 'V': 5, 'W': 6, 'X': 7, 'Y': 8, 'Z': 9
+    }
     weights = [8, 7, 6, 5, 4, 3, 2, 10, 0, 9, 8, 7, 6, 5, 4, 3, 2]
     
     # Calcular el valor del VIN
     sum = 0
     for i in range(len(vin)):
-        value = vin_map.index(vin[i])
-        sum += value * weights[i]
+        char = vin[i]
+        if char in vin_values:
+            value = vin_values[char]
+            sum += value * weights[i]
     
     # Calcular dígito de verificación
     check_digit = sum % 11
