062f65091579b6967b3692831c7b5b4464f197ca
raulvazquez-wheelz <148226717+raulvazquez-wheelz@users.noreply.github.com>
2024-05-16 11:07:06 +0200

    Update date.py

--- src/wheelz_idp_validations/sanitizers/date.py
+++ src/wheelz_idp_validations/sanitizers/date.py
@@ -2,7 +2,7 @@ import re
 from datetime import datetime
 from ..exceptions.date_exceptions import *
 
-def sanitize_dates(received_date):
+def sanitize_dates(received_date, multiple=False):
     # Comprobar si la entrada es una cadena vacía o solo contiene espacios
     if not received_date.strip():
         raise EmptyDateException(received_date)
@@ -11,12 +11,20 @@ def sanitize_dates(received_date):
     date_pattern = r'\b(\d{2})[\/\s-](\d{2})[\/\s-](\d{4})\b'
     
     try:
-        match = re.search(date_pattern, received_date)
-        if match:
-            day, month, year = match.groups()
-            iso_formatted_date = f"{year}-{month}-{day}"
-            datetime.strptime(iso_formatted_date, "%Y-%m-%d")  # Validar fecha convertida
-            return iso_formatted_date
+        matches = re.findall(date_pattern, received_date)
+        if matches:
+            formatted_dates = []
+            for match in matches:
+                day, month, year = match
+                iso_formatted_date = f"{year}-{month}-{day}"
+                # Validar fecha convertida
+                datetime.strptime(iso_formatted_date, "%Y-%m-%d")
+                formatted_dates.append(iso_formatted_date)
+                
+            if multiple:
+                return formatted_dates  # Devuelve todas las fechas formateadas
+            else:
+                return formatted_dates[0] if formatted_dates else None  # Devuelve solo la primera fecha formateada
         else:
             # Lanzar una excepción si no se encuentra una fecha válida
             raise InvalidDateException(received_date)
