5637bb3d5e2bbf639c8c40448c704d84e153e3ed
raulvazquez-wheelz <148226717+raulvazquez-wheelz@users.noreply.github.com>
2024-05-16 11:08:54 +0200

    Update pcv2_sanitizer.py

--- src/wheelz_idp_validations/pcv2_sanitizer.py
+++ src/wheelz_idp_validations/pcv2_sanitizer.py
@@ -26,7 +26,7 @@ def sanitize_pcv2(response_dict):
     # Sanitize field fecha de primera matriculación. Campo B
     if "B" in response_dict:
         try:
-            response_dict["B"] = sanitize_dates(response_dict["B"])
+            response_dict["B"] = sanitize_dates(response_dict["B"], multiple = False)
         except Exception as e:
             response_dict["B"] = ""
             print("Error sanitizing field B:", str(e))
@@ -36,7 +36,7 @@ def sanitize_pcv2(response_dict):
     # Sanitize field periodo de validez del permiso. Campo H
     if "H" in response_dict:
         try:
-            response_dict["H"] = sanitize_dates(response_dict["H"])
+            response_dict["H"] = sanitize_dates(response_dict["H"], multiple = False)
         except Exception as e:
             response_dict["H"] = ""
             print("Error sanitizing field H:", str(e))
@@ -46,7 +46,7 @@ def sanitize_pcv2(response_dict):
     # Sanitize field fecha de matriculación a la que se refiere el presente permiso. Campo I
     if "I" in response_dict:
         try:
-            response_dict["I"] = sanitize_dates(response_dict["I"])
+            response_dict["I"] = sanitize_dates(response_dict["I"], multiple = False)
         except Exception as e:
             response_dict["I"] = ""
             print("Error sanitizing field I:", str(e))
@@ -56,7 +56,7 @@ def sanitize_pcv2(response_dict):
     # Sanitize field fecha de expedición. Campo I.1
     if "I.1" in response_dict:
         try:
-            response_dict["I.1"] = sanitize_dates(response_dict["I.1"])
+            response_dict["I.1"] = sanitize_dates(response_dict["I.1"], multiple = False)
         except Exception as e:
             response_dict["I.1"] = ""
             print("Error sanitizing field I.1:", str(e))
